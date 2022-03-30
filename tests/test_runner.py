eec0e4428c7ac6d562d3773fb5c19f4f59d40f84
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2022-03-31 00:05:11 +0500

    union mock and connected tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,7 +3,6 @@ from testtoposorter import TestTopoSorter
 from testsqlservertemplates import TestSqlServerTemplates
 from testsqlquerybuilder import TestSqlQueryBuilder
 from testdbtable import TestDbTable
-from testdbtableconnected import TestDbTableConnected
 
 
 suite = unittest.TestSuite()
@@ -11,7 +10,6 @@ suite.addTest(unittest.makeSuite(TestTopoSorter))
 suite.addTest(unittest.makeSuite(TestSqlServerTemplates))
 suite.addTest(unittest.makeSuite(TestSqlQueryBuilder))
 suite.addTest(unittest.makeSuite(TestDbTable))
-suite.addTest(unittest.makeSuite(TestDbTableConnected))
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(suite)
--- tests/testdbtable.py
+++ tests/testdbtable.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from core.dbtable import DbTable
 from core.sqlquerybuilder import SqlQueryBuilder
 from core.sqlservertemplates import SqlServerTemplates
+from dbconnector import DbConnector
+
 
 LOGGER_DICT_STUB = {
     "version": 1,
@@ -22,12 +24,15 @@ LOGGER_DICT_STUB = {
         }
     }
 }
+IS_CONNECTED = DbConnector().check_and_close_connection()
+WORK_DB_NAME = "UnitTest_DbTable_Work"
+CLEAR_DB_NAME = "UnitTest_DbTable_Clear"
 TABLE_NAME = "dbo.test"
 PRIMARY_KEY_COL = "test_id"
-UPDATE_DT_COL = "test_upddt"
 INT_COL = "test_int"
 FLOAT_COL = "test_float"
 STR_COL = "test_str"
+UPDATE_DT_COL = "test_upddt"
 COLUMNS = [PRIMARY_KEY_COL, INT_COL, FLOAT_COL, STR_COL, UPDATE_DT_COL]
 STR_COLUMNS = ",".join(COLUMNS)
 LINK_COLUMNS = (',\n' + ' ' * 12).join(["trg.{0} = src.{0}".format(col)
@@ -36,53 +41,144 @@ LINK_COLUMNS = (',\n' + ' ' * 12).join(["trg.{0} = src.{0}".format(col)
 INS_COLUMNS = ",".join(["src.{0}".format(col) for col in COLUMNS])
 DT = datetime.now()
 DT_STR = DT.isoformat(sep=' ', timespec='milliseconds')
-WORK_DB_NAME = "work"
-CLEAR_DB_NAME = "clear"
+
+CREATE_TABLE_SCRIPT = f"""
+CREATE TABLE {TABLE_NAME}(
+    [{PRIMARY_KEY_COL}] [bigint] IDENTITY(1,1) NOT NULL,
+    [{INT_COL}] [bigint] NULL,
+    [{FLOAT_COL}] [float] NULL,
+    [{STR_COL}] [varchar](100) NULL,
+    [{UPDATE_DT_COL}] [datetime] NULL,
+ CONSTRAINT [PK_test] PRIMARY KEY CLUSTERED ([{PRIMARY_KEY_COL}] ASC)
+) ON [PRIMARY];"""
+
+CREATE_DB_SCRIPT = f"""
+CREATE DATABASE [{WORK_DB_NAME}];
+CREATE DATABASE [{CLEAR_DB_NAME}];"""
+
+INIT_SCRIPT = f"""
+USE [{WORK_DB_NAME}];
+{CREATE_TABLE_SCRIPT}
+USE [{CLEAR_DB_NAME}];
+{CREATE_TABLE_SCRIPT}"""
+
+TRUNCATE_SCRIPT = f"""
+TRUNCATE TABLE {WORK_DB_NAME}.{TABLE_NAME};
+TRUNCATE TABLE {CLEAR_DB_NAME}.{TABLE_NAME};
+"""
+
+DROP_SCRIPT = f"""
+USE [master];
+DROP DATABASE [{WORK_DB_NAME}];
+DROP DATABASE [{CLEAR_DB_NAME}];"""
 
 
 class TestDbTable(unittest.TestCase):
     templates = SqlServerTemplates()
-    cursor = MagicMock()
-    cursor.execute = MagicMock(return_value=None)
-    cursor.fetchall = MagicMock(return_value=
-                                [[col,
-                                  1 if col == UPDATE_DT_COL else 0,
-                                  1 if col == PRIMARY_KEY_COL else 0]
-                                 for col in COLUMNS])
     queries = SqlQueryBuilder(templates)
-    table = DbTable(LOGGER_DICT_STUB, cursor, queries, TABLE_NAME, WORK_DB_NAME,
-                    CLEAR_DB_NAME)
+    connector = None
+    cursor = None
+    table = None
+    mock_cursor = MagicMock()
+    mock_cursor.execute = MagicMock(return_value=None)
+    mock_cursor.fetchall = MagicMock(return_value=
+                                     [[col,
+                                       1 if col == UPDATE_DT_COL else 0,
+                                       1 if col == PRIMARY_KEY_COL else 0]
+                                      for col in COLUMNS])
+    mock_table = DbTable(LOGGER_DICT_STUB, mock_cursor, queries, TABLE_NAME,
+                         WORK_DB_NAME, CLEAR_DB_NAME)
 
+    @classmethod
+    def setUpClass(cls):
+        if not IS_CONNECTED:
+            return
+        cls.connector = DbConnector()
+        cls.cursor = cls.connector.get_cursor()
+        cls.cursor.execute(CREATE_DB_SCRIPT)
+        cls.cursor.execute(INIT_SCRIPT)
+        cls.table = DbTable(LOGGER_DICT_STUB, cls.cursor, cls.queries,
+                            TABLE_NAME, WORK_DB_NAME, CLEAR_DB_NAME)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.connector:
+            cls.cursor.execute(DROP_SCRIPT)
+            cls.connector.close()
+
+    def setUp(self):
+        self.cursor.execute(TRUNCATE_SCRIPT)
+
+    def __insert_data(self, database_name, values):
+        ins_query = f"""
+        SET IDENTITY_INSERT {database_name}.{TABLE_NAME} ON;
+        INSERT INTO {database_name}.{TABLE_NAME}({STR_COLUMNS})
+        VALUES {values};
+        SET IDENTITY_INSERT {database_name}.{TABLE_NAME} OFF;
+        """
+        self.cursor.execute(ins_query)
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test__init__(self):
-        table = DbTable(LOGGER_DICT_STUB, self.cursor, self.queries, TABLE_NAME,
-                        WORK_DB_NAME, CLEAR_DB_NAME)
+        self.assertEqual(self.table.name, TABLE_NAME)
+
+    def test__init__mock(self):
+        table = DbTable(LOGGER_DICT_STUB, self.mock_cursor, self.queries,
+                        TABLE_NAME, WORK_DB_NAME, CLEAR_DB_NAME)
         self.assertEqual(table.name, TABLE_NAME)
 
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_delete_statement_list_empty(self):
-        self.cursor.fetchall = MagicMock(return_value=[])
         self.assertEqual(self.table.get_delete_statement_list(), [])
 
+    def test_get_delete_statement_list_empty_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[])
+        self.assertEqual(self.mock_table.get_delete_statement_list(), [])
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_delete_statement_list_single(self):
-        self.cursor.fetchall = MagicMock(return_value=[[123456789]])
+        values = f"(1,1,1.1,'test','{DT_STR}')"
+        self.__insert_data(CLEAR_DB_NAME, values)
+        str_id_list = "1"
+        statement = self.templates.delete_statement.format(TABLE_NAME,
+                                                           PRIMARY_KEY_COL,
+                                                           str_id_list)
+        self.assertEqual(self.table.get_delete_statement_list(), [statement])
+
+    def test_get_delete_statement_list_single_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[[123456789]])
         str_id_list = "123456789"
         statement = self.templates.delete_statement.format(TABLE_NAME,
                                                            PRIMARY_KEY_COL,
                                                            str_id_list)
-        self.assertEqual(self.table.get_delete_statement_list(), [statement])
+        self.assertEqual(self.mock_table.get_delete_statement_list(),
+                         [statement])
 
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_delete_statement_list_multi(self):
-        self.cursor.fetchall = MagicMock(return_value=[[123456789], [987654321],
-                                                       [111111111]])
-        str_id_list = "123456789,987654321,111111111"
+        values = "(1,1,1.1,'',null),(2,1,1.1,'',null),(3,1,1.1,'',null)"
+        self.__insert_data(CLEAR_DB_NAME, values)
+        str_id_list = "1,2,3"
         statement = self.templates.delete_statement.format(TABLE_NAME,
                                                            PRIMARY_KEY_COL,
                                                            str_id_list)
         self.assertEqual(self.table.get_delete_statement_list(), [statement])
 
+    def test_get_delete_statement_list_multi_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[[123456789],
+                                                            [987654321],
+                                                            [111111111]])
+        str_id_list = "123456789,987654321,111111111"
+        statement = self.templates.delete_statement.format(TABLE_NAME,
+                                                           PRIMARY_KEY_COL,
+                                                           str_id_list)
+        self.assertEqual(self.mock_table.get_delete_statement_list(), [statement])
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_delete_statement_list_multi_row_limit(self):
-        self.cursor.fetchall = MagicMock(return_value=[[123456789], [987654321],
-                                                       [111111111]])
-        str_id_list = ["123456789", "987654321", "111111111"]
+        values = "(1,1,1.1,'',null),(2,1,1.1,'',null),(3,1,1.1,'',null)"
+        self.__insert_data(CLEAR_DB_NAME, values)
+        str_id_list = ["1", "2", "3"]
         statements = [
             self.templates.delete_statement.format(TABLE_NAME, PRIMARY_KEY_COL,
                                                    num)
@@ -90,13 +186,42 @@ class TestDbTable(unittest.TestCase):
         ]
         self.assertEqual(self.table.get_delete_statement_list(1), statements)
 
+    def test_get_delete_statement_list_multi_row_limit_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[[123456789],
+                                                            [987654321],
+                                                            [111111111]])
+        str_id_list = ["123456789", "987654321", "111111111"]
+        statements = [
+            self.templates.delete_statement.format(TABLE_NAME, PRIMARY_KEY_COL,
+                                                   num)
+            for num in str_id_list
+        ]
+        self.assertEqual(self.mock_table.get_delete_statement_list(1),
+                         statements)
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_upsert_statement_list_empty(self):
-        self.cursor.fetchall = MagicMock(return_value=[])
         self.assertEqual(self.table.get_upsert_statement_list(), [])
 
+    def test_get_upsert_statement_list_empty_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[])
+        self.assertEqual(self.mock_table.get_upsert_statement_list(), [])
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_upsert_statement_list_single_row(self):
-        self.cursor.fetchall = MagicMock(return_value=[[123456789, 123, 1.23,
-                                                        "test", DT]])
+        values = "(123456789,123,1.23,'test','2022-03-30 23:19:14.777')"
+        self.__insert_data(WORK_DB_NAME, values)
+        statement = self.templates.upsert_statement.format(TABLE_NAME,
+                                                           STR_COLUMNS,
+                                                           values,
+                                                           PRIMARY_KEY_COL,
+                                                           LINK_COLUMNS,
+                                                           INS_COLUMNS)
+        self.assertEqual(self.table.get_upsert_statement_list(), [statement])
+
+    def test_get_upsert_statement_list_single_row_mock(self):
+        self.mock_cursor.fetchall = MagicMock(return_value=[[123456789, 123,
+                                                             1.23, "test", DT]])
         values = f"(123456789,123,1.23,'test','{DT_STR}')"
         statement = self.templates.upsert_statement.format(TABLE_NAME,
                                                            STR_COLUMNS,
@@ -104,17 +229,15 @@ class TestDbTable(unittest.TestCase):
                                                            PRIMARY_KEY_COL,
                                                            LINK_COLUMNS,
                                                            INS_COLUMNS)
-        self.assertEqual(self.table.get_upsert_statement_list(), [statement])
+        self.assertEqual(self.mock_table.get_upsert_statement_list(),
+                         [statement])
 
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_upsert_statement_list_multi_rows(self):
-        return_list = [[123456787, 123, 1.23, "test", DT],
-                       [123456788, None, 1.23, "'quoted'", None],
-                       [123456789, 123, None, "test", DT]]
-        self.cursor.fetchall = MagicMock(return_value=return_list)
-        values = f"(123456787,123,1.23,'test','{DT_STR}')" + ',\n'+' ' * 8 +\
+        values = f"(123456787,123,1.23,'test',null)" + ',\n'+' ' * 8 +\
                  "(123456788,null,1.23,'''quoted''',null)" + ',\n'+' ' * 8 +\
-                 f"(123456789,123,null,'test','{DT_STR}')"
-
+                 f"(123456789,123,null,'test',null)"
+        self.__insert_data(WORK_DB_NAME, values)
         statement = self.templates.upsert_statement.format(TABLE_NAME,
                                                            STR_COLUMNS,
                                                            values,
@@ -123,10 +246,42 @@ class TestDbTable(unittest.TestCase):
                                                            INS_COLUMNS)
         self.assertEqual(self.table.get_upsert_statement_list(), [statement])
 
+    def test_get_upsert_statement_list_multi_rows_mock(self):
+        return_list = [[123456787, 123, 1.23, "test", DT],
+                       [123456788, None, 1.23, "'quoted'", None],
+                       [123456789, 123, None, "test", DT]]
+        self.mock_cursor.fetchall = MagicMock(return_value=return_list)
+        values = f"(123456787,123,1.23,'test','{DT_STR}')" + ',\n'+' ' * 8 +\
+                 "(123456788,null,1.23,'''quoted''',null)" + ',\n'+' ' * 8 +\
+                 f"(123456789,123,null,'test','{DT_STR}')"
+        statement = self.templates.upsert_statement.format(TABLE_NAME,
+                                                           STR_COLUMNS,
+                                                           values,
+                                                           PRIMARY_KEY_COL,
+                                                           LINK_COLUMNS,
+                                                           INS_COLUMNS)
+        self.assertEqual(self.mock_table.get_upsert_statement_list(),
+                         [statement])
+
+    @unittest.skipIf(not IS_CONNECTED, "Is not connected")
     def test_get_upsert_statement_list_multi_row_limit(self):
+        values = ["(123456787,123,1.23,'test',null)",
+                  "(123456788,null,1.23,'''quoted''',null)"]
+        self.__insert_data(WORK_DB_NAME, ','.join(values))
+        statements = [self.templates.upsert_statement.format(TABLE_NAME,
+                                                             STR_COLUMNS,
+                                                             value,
+                                                             PRIMARY_KEY_COL,
+                                                             LINK_COLUMNS,
+                                                             INS_COLUMNS)
+                      for value in values]
+        self.assertEqual(self.table.get_upsert_statement_list(row_limit=1),
+                         statements)
+
+    def test_get_upsert_statement_list_multi_row_limit_mock(self):
         return_list = [[123456787, 123, 1.23, "test", DT],
                        [123456788, None, 1.23, "'quoted'", None]]
-        self.cursor.fetchall = MagicMock(return_value=return_list)
+        self.mock_cursor.fetchall = MagicMock(return_value=return_list)
         values = [f"(123456787,123,1.23,'test','{DT_STR}')",
                   "(123456788,null,1.23,'''quoted''',null)"]
         statements = [self.templates.upsert_statement.format(TABLE_NAME,
@@ -136,7 +291,7 @@ class TestDbTable(unittest.TestCase):
                                                              LINK_COLUMNS,
                                                              INS_COLUMNS)
                       for value in values]
-        self.assertEqual(self.table.get_upsert_statement_list(row_limit=1),
+        self.assertEqual(self.mock_table.get_upsert_statement_list(row_limit=1),
                          statements)
 
 
