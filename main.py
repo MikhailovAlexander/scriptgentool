73fb5bd8e63e7c7889c86603423234d30339f82a
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2022-04-08 23:03:14 +0500

    update main file

--- main.py
+++ main.py
@@ -1,43 +1,17 @@
 import os
 from typing import Any, Sequence
-from pyodbc import Connection, Cursor, connect
+import pyodbc
 import logging
 import logging.config
 import json
 import argparse
 
+from core.sqlservertemplates import SqlServerTemplates
+from core.sqlquerybuilder import SqlQueryBuilder
 from core.scriptgenerator import ScriptGenerator
 
 LOG_CONF_FILE_PATH = "config/logger_conf.json"
 APP_CONF_FILE_PATH = "config/app_conf.json"
-
-
-def load_json(json_file_path: str) -> dict[str: Any]:
-    """Reads a data from the json file.
-
-    :param json_file_path: the file path for data loading.
-    :return: a json data.
-    """
-
-    with open(json_file_path, 'r') as conf_file:
-        return json.load(conf_file)
-
-
-def get_connection(conn_config: dict[str: str]) -> Connection:
-    """Opens the database connection with parameters from the config dictionary.
-
-    :param conn_config: a dictionary with connection parameters.
-    :return: a Connection object.
-    """
-
-    db_serv = conn_config["server"]
-    clear_db_name = conn_config["clear_db_name"]
-    db_user = conn_config["user"]
-    db_pass = conn_config["password"]
-    con_str = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={db_serv};" +\
-              f";DATABASE={clear_db_name};UID={db_user};PWD={db_pass};" \
-              f"TrustServerCertificate=Yes"
-    return connect(con_str)
 
 
 def parse_args(script_config: dict[str: Any]) -> argparse.Namespace:
@@ -92,8 +66,9 @@ def check_table_settings(table_settings: dict[str: Any]) -> None:
 
 
 def main():
-    """Основная функция для выгрузки изменений по списку таблиц"""
-    log_config = load_json(LOG_CONF_FILE_PATH)
+    log_config = None
+    with open(LOG_CONF_FILE_PATH, 'r') as conf_file:
+        log_config = json.load(conf_file)
     file_path = None
     try:
         file_path = log_config["handlers"]["file_handler"]["filename"]
@@ -108,23 +83,24 @@ def main():
     logger.info("Start app")
     connection = None
     cursor = None
+
     try:
-        app_config = load_json(APP_CONF_FILE_PATH)
-        args = parse_args(app_config["script_settings"])
-        logger.info(f"args: all: {args.all}, days: {args.days}, "
-                    f"rows: {args.rows}, size: {args.size}")
-        clear_db_name = app_config["connection"]["clear_db_name"]
-        work_db_name = app_config["connection"]["work_db_name"]
-        git_folder_path = app_config["repository"]["git_folder_path"]
-        target_folder = app_config["repository"]["target_folder"]
+        app_config = None
+        with open(APP_CONF_FILE_PATH, 'r') as conf_file:
+            app_config = json.load(conf_file)
         table_settings = app_config["table_settings"]
         check_table_settings(table_settings)
-        connection = get_connection(app_config["connection"])
+        connection = pyodbc.connect(app_config["connection"]["conn_string"])
         cursor = connection.cursor()
-        generator = ScriptGenerator(log_config, cursor, work_db_name,
-                                    clear_db_name, git_folder_path,
-                                    target_folder, table_settings,
+        query_builder = SqlQueryBuilder(SqlServerTemplates())
+        generator = ScriptGenerator(log_config, cursor, query_builder,
+                                    app_config["connection"]["work_db_name"],
+                                    app_config["connection"]["clear_db_name"],
+                                    app_config["repository"]["git_folder_path"],
+                                    app_config["repository"]["target_folder"],
+                                    table_settings,
                                     app_config["liquibase_settings"])
+        args = parse_args(app_config["script_settings"])
         if args.all:
             message = app_config["script_settings"]["upload_message"]
             generator.upload_tables(args.size, message, args.rows)
@@ -140,3 +116,6 @@ def main():
         if connection:
             connection.close()
         logger.info('Connection close')
+
+
+main()
