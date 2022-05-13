192155c68fda14f5c9eafe96d104479515fffa93
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2022-05-13 22:29:33 +0500

    update scripts for the integration tests

--- integr_tests/insert6.sql
+++ integr_tests/insert6.sql
@@ -2,5 +2,5 @@ SET IDENTITY_INSERT IntegrTestWork.dbo.Catalog ON;
 INSERT INTO IntegrTestWork.dbo.Catalog(
     Catalog_id, Catalog_pid, Catalog_Name, Catalog_insDT, Catalog_updDT)
 VALUES (1,NULL,'to insert 1',getdate(),getdate()),
-       (2,NULL,'to insert 2',dateadd(day, -1, getdate()),dateadd(day, -1, getdate()));
+       (2,NULL,'to insert 2',dateadd(day, -2, getdate()),dateadd(day, -2, getdate()));
 SET IDENTITY_INSERT IntegrTestWork.dbo.Catalog OFF;
